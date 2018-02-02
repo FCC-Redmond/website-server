f6a9ed8973e35636110c035bff41e08cb8dc6a99
Apoorva Teja <apoorva.teja@gmail.com>
2018-02-01 20:22:43 -0800

    fixed server.js and added gitignore

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const app express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // mongodb connection

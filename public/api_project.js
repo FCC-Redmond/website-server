17b7d5c93eedbe5ebc082c915a9cc2d343e38260
Apoorva Teja <apoorva.teja@gmail.com>
2018-02-13 14:03:45 -0800

    Updated API docs

--- public/api_project.js
+++ public/api_project.js
@@ -1,15 +1,14 @@
 define({
-  "name": "fccRedmond",
-  "version": "0.0.1",
-  "description": "FreeCodeCamp Redmond chpter website server API",
-  "title": "FreeCodeCamp Redmond Server API",
+  "name": "FCCRedmond Server API",
+  "version": "0.0.2",
+  "description": "FreeCodeCamp Redmond Chapter Website support API for member management ",
   "url": "https://fccredmond.herokuapp.com",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-02-13T02:10:53.623Z",
+    "time": "2018-02-13T22:02:46.895Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }

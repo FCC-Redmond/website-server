ec2256b03c9639ec164b18e393a8136ef1b248b5
Apoorva Teja <apoorva.teja@gmail.com>
2018-02-16 12:26:14 -0800

    more debug logs to figureout what's happening in PROD

--- routes/api.js
+++ routes/api.js
@@ -431,8 +431,8 @@ var updateMember = async function (req, res, next) {
                     onError(res, error, 500);
                     return;
                 } else if (!updatedMemberProfile) {
-                    console.log("error:" + error);
-                    console.log("memberProfile:"+ memberProfile);
+                    console.log("Inside api.js update. error:" + error);
+                    console.log("Inside api.js update. memberProfile:"+ updatedMemberProfile);
                     onError(res, new Error("Invalid ID sent. Nothing was updated"), 400);
                 } else {
                     res.status(200).send({

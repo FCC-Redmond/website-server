1b5c61f7e5c47c984b172eb4d4b161b66d0cc61e
Apoorva Teja <apoorva.teja@gmail.com>
2018-03-22 20:12:07 -0700

    Fixed api docs

--- public/api_project.js
+++ public/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-03-23T03:07:12.156Z",
+    "time": "2018-03-23T03:11:20.370Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
